d35ba3a1053961fdbf38af3dea831491d26b4a40
zun95 <ilvinchan95@hotmail.com>
2019-05-18 17:54:23 +0900

    タスク完成

--- submit/16C1010009 Chan/q2_1.rb
+++ submit/16C1010009 Chan/q2_1.rb
@@ -6,8 +6,19 @@ if rands >=4 && rands <= 10
 	puts "おはよう"
 elsif rands >= 11 && rands <= 18
 	puts "こんにちは"
-else
-	case rands
+elsif rands >= 19 && rands <= 24
+	puts "こんばんは"
+elsif rands >= 1 && rands <= 3
+	puts "こんばんは"
+else 
+	puts "エラー"
+end
+
+case rands
+	when 4...12
+		puts "おはよう"
+	when 11...19
+		puts "こんにちは"
 	when 19...25
 		puts "こんばんは"
 	when 1...4
@@ -15,4 +26,3 @@ else
 	else
 		puts "エラー"
 	end
-end
