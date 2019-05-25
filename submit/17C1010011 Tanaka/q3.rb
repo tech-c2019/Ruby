fd755a2990b077841a92816a795d1fd1de2285c8
tarezou <17c1010011th@edu.tech.ac.jp>
2019-05-25 17:45:18 +0900

    q3 commit

---
+++ submit/17C1010011 Tanaka/q3.rb
@@ -0,0 +1,28 @@
+
+5.times{
+	puts "hello world\n"
+}
+
+puts ''
+
+(1..9).each{|num01|
+	(1..9).each{|num02|
+		ans = (num01 * num02)
+		printf("%02d ",ans)
+	}
+	puts ''
+}
+
+puts ''
+
+(1..100).each{|num|
+	if num % 3 == 0 && num % 5 == 0
+		puts "FizeBazz"
+	elsif num % 3 == 0
+		puts "Fizz"
+	elsif num % 5 == 0
+		puts "Buzz"
+	else
+		puts num
+	end
+}
