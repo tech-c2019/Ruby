849e1144ac4c9eeaba1e9e31f03b205200ddc60f
zdh10000 <49806711+zdh10000@users.noreply.github.com>
2019-05-27 11:49:00 +0900

    Add files via upload

---
+++ submit/17D2030004 Zhu/q2.rb
@@ -0,0 +1,48 @@
+numbercase = ARGV[1].to_i
+print "数値入力:",ARGV[1],"  "
+case numbercase
+	when 1..3
+	puts "こんばんは"
+	when 19..24
+	puts "こんばんは"
+	when 4..10
+	puts "おはよう"
+	when 11..18
+	puts "こんにちは"
+end
+----------------------------------------
+numberif = ARGV[0].to_i
+print "数値入力:",ARGV[0],"  "
+
+if numberif > 0 && numberif < 4
+	puts "こんばんは"
+	elsif numberif > 18 && numberif < 25
+	puts "こんばんは"
+	elsif numberif > 3 && numberif < 11
+	puts "おはよう"
+	elsif numberif > 10 && numberif < 19
+	puts "こんにちは"
+end
+------------------------------------------
+m=ARGV[3].to_f
+kg=ARGV[4].to_f
+print "入力情報 m",ARGV[3],"  "
+print "入力情報 kg",ARGV[4],"  "
+
+bmi=kg/(m*m)
+if bmi>18.5
+	puts "低体重"
+elsif bmi>40
+	puts "肥満(4度)"
+end
+
+case bmi
+	when 18.5..25.0
+	puts "普通体重"
+	when 25.1..30.0
+	puts "肥満(1度）"
+	when 30.1..35.0
+	puts "肥満(2度)"
+	when 35.1..40.0
+	puts "肥満(3度)"
+end
