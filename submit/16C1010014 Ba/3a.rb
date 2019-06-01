3c670ef17204240010c10c54420073de650351c8
Basan <basanpanthin007@gmail.com>
2019-06-01 18:37:48 +0900

    aaaaa

--- submit/16C1010014 Ba/3a.rb
+++ submit/16C1010014 Ba/3a.rb
@@ -1,3 +1,46 @@
+‰Û‘è1
 5.times {
 print ("hello world\n")
 } 
+
+
+
+
+#‰Û‘è2
+number=ARGV[0].to_i
+
+number==1 && number <=100 
+
+
+   if number%15==0
+        puts "FizzBuzz!"
+    elsif number %3==0
+        puts "Fizz!"
+    elsif number %5==0 
+        puts "Buzz!"
+    else
+        number
+    end
+
+
+
+
+
+‰Û‘è3
+for a in 1..9
+puts ""
+for b in 1..9
+
+
+	if a*b<10
+		print "0",a*b,"  "
+	
+	
+	else
+	print a*b,"  "
+	
+	end
+end
+end
+ 
+ 
