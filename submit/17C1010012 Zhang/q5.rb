02b49da9419562ae8bbce60e34fb70bdfb7b97e1
cyousan <cyouyuhan@gmail.com>
2019-06-22 16:05:27 +0900

    add q5

---
+++ submit/17C1010012 Zhang/q5.rb
@@ -0,0 +1,32 @@
+## 1.Hogeモジュールを利用しクラスメソッドfooを持つTestクラスを定義
+module Hoge
+  def foo
+    "Foo" + bar
+  end
+
+  private
+  def bar
+    "Bar"
+  end
+end
+
+class Test
+    extend Hoge
+    def foo
+        foo
+    end
+end
+
+puts Test.foo
+
+## 2.下記コードは実行エラーとなる、特異メソッドを使い実行結果の出力となるように修正せよ
+
+hello = "hello world"
+
+class << hello
+    def print
+        puts "data: #{self}"
+    end
+end
+
+hello.print
