540d17ba8604e89fa104ac4dae19312bceb79324
soepyaehtike <soepyae@gmail.com>
2019-06-22 18:50:46 +0900

    add q6.rb

---
+++ submit/17C1010010 Soe Pyae Htike/q6.rb
@@ -0,0 +1,62 @@
+class QuestionSix
+
+     def division(a,b)
+        begin
+            if b == 0 then
+                raise
+            end
+            r=a/b
+        rescue
+            r=0
+        end
+        return r
+    end
+
+
+     def discount(*arr)
+        idx = 0
+        total = 0
+        while idx < arr.size
+          total +=  yield(arr[idx])
+          idx += 1
+        end
+        return total.to_i
+    end
+    def inputForm()
+        p "Please enter up to 5 prices(The end is end):"
+        total = 0
+        count = 0
+        input = ""
+        array = []
+        while input != "end" do
+            flag = false
+            begin
+                input = gets.chomp
+                if input == "end" then
+                    break
+                end
+                if input.to_i <= 0  then
+                    raise
+                end
+            rescue
+                p input + "it is fraud。Please enter the correct number"
+            retry
+            end
+
+             total += input.to_i
+            array.push(input.to_i)
+            count += 1
+
+             if count > 4 then
+                break
+            end
+        end
+
+         puts "The total is" + total.to_s + "The total price after discounting in yen is" + self.discount(*array){|num| num * 0.8}.to_s + "en"
+    end
+end
+
+
+ qSix = QuestionSix.new
+p qSix.discount(1000,2000,3000){|num| num * 0.8}
+qSix.inputForm() 
