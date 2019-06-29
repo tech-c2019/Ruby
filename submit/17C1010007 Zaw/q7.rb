f44d10bbfe6c42279eb504fa61fc02069e1fac41
hoshime <youtuber@.com>
2019-06-29 18:39:31 +0900

    add q7

---
+++ submit/17C1010007 Zaw/q7.rb
@@ -0,0 +1,22 @@
+#1
+ary = (1..100).to_a
+
+ary2 = ary.select{|i| i % 3 == 0 }
+p ary2
+
+#2
+def sum_array(ary1, ary3)
+  result = Array.new
+  ary1.zip(ary3){|a, b| result << a + b }
+  return result
+end
+p sum_array([1, 2, 3], [4, 6, 8])
+
+#3
+list = [1,2,3,1,2,3,1,2,3,"a","java","java","c","c"]#同じnumber を削除
+p list.uniq
+
+#4
+list = [6,1,4,2,9,7,8,12,5,20,3]
+list.sort! 
+p list
