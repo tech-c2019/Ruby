270de96eb5d565218e4672bff36cfd1d1782f26a
Moyashinn <yasaiseikatu2525@gmail.com>
2019-07-06 18:02:59 +0900

    anser Q8

---
+++ submit/16C1020015 Takeisi/Q8.rb
@@ -0,0 +1,33 @@
+#1
+to_eng_month = {
+    1 => "January",
+    2 => "Febrary",
+    3 => "March",
+    4 => "April",
+    5 => "May",
+    6 => "Jun",
+    7 => "Jul",
+    8 => "August",
+    9 => "September",
+    10 => "October",
+    11 => "Nobember",
+    12 => "December"
+    }
+
+p to_eng_month[1]
+
+
+#-------------------------------
+#2
+list = {apple: 100,banana: 500, pynappel:400, meron:200}
+list.delete_if do|key,value|
+    value > 300
+end
+p list
+
+#-------------------------------
+#3
+fl = {apple: 100,banana: 500, pynappel: 400, meron: 200}
+fl.each do |key,value|
+    print "#{key} = #{value} yen \n"
+end
