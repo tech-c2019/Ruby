26c453c58dac9f3630c8ac6eb4a662657db82ab9
Osanai-M <17c1010004om@edu.tech.ac.jp>
2019-07-06 18:45:39 +0900

    Q8.rb modified

--- submit/17c1010004 osanai/Q8.rb
+++ submit/17c1010004 osanai/Q8.rb
@@ -1,7 +1,8 @@
-# Q7.rb
+# Q8.rb
+# 20190706
 
-## Q7-01
-puts "Q7-01"
+## Q8-01
+puts "Q8-01"
 
 def to_eng_month(num)
     month = {
@@ -18,8 +19,8 @@ p to_eng_month(1)
 puts "\n"
 
 
-## Q7-02
-puts "Q7-02"
+## Q8-02
+puts "Q8-02"
 
 ar = {apple: 100,banana: 500,pynappel: 400,meron: 200}
 ar.delete_if do |key, value|
@@ -31,8 +32,8 @@ p ar
 puts "\n"
 
 
-## Q7-03
-puts "Q7-03"
+## Q8-03
+puts "Q8-03"
 
 fruits = {apple: 100,banana: 500,pynappel: 400,meron: 200}
 sum = 0
@@ -42,3 +43,4 @@ fruits.each do |key, value|
     sum += value
 end
 print "sum = #{sum} yen"
+
