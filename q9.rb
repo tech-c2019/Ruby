65f1a92ac2da9af697fa9563b658b5fb91b91c42
Chenhan1123 <49807138+Chenhan1123@users.noreply.github.com>
2019-07-20 17:59:44 +0900

    q9

---
+++ q9.rb
@@ -0,0 +1,64 @@
+name = nil
+phone =nil
+code =nil
+email=nil
+
+#名前
+loop do
+p 'fill user'
+name = gets.to_s
+
+    if %r!^@[A-Za-z]{3,}$! =~ name
+    puts "match"
+    break;
+        end
+        puts"invalid"
+
+end    
+
+
+#電話番号
+loop do
+p 'fill phone number '
+phone = gets.to_s
+
+    if %r!^0\d{1,4}-\d{3,4}-\d{3,4}$! =~ phone
+    puts "match"
+    break;
+        end
+        puts"invalid"
+
+end    
+
+#郵便番号
+loop do
+p 'fill post code'
+code = gets.to_s
+
+    if %r!^[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]! =~ code
+    puts "match"
+        break;
+        end
+        puts"invalid"
+
+end
+
+#メールアドレス
+loop do
+p 'fill email'
+email = gets.to_s
+
+    if %r!^[A-Za-z0-9]+[\w-]+@[\w\.-]+\.\w{2,}$! =~ email
+    puts "match"
+        break;
+        end
+        puts"invalid"
+
+end    
+
+print "Here your data\n"
+
+puts "your name  #{name}"
+puts "your phone number #{phone}"
+puts "your Zip code #{code}"
+puts "your email #{email}"
