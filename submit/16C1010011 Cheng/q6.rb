009ec346f959b5473ea06be190a48f730051d3ac
kaka77solo <39084144+kaka77solo@users.noreply.github.com>
2019-07-20 18:32:52 +0900

    Add files via upload

---
+++ submit/16C1010011 Cheng/q6.rb
@@ -0,0 +1,19 @@
+    
+class Eroor < StandardError
+    def message
+        "saduashufbg"
+    end
+end
+
+ a = gets.to_i
+b = gets.to_i
+
+ begin
+    num = (a / b)
+    raise Eroor    
+rescue => exception
+    num = 0
+    puts exception.message
+ensure
+    puts num
+end  
